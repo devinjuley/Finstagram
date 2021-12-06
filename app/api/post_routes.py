1d18834cc48671b9eadc525f2b07e74bc9771b93
mehendaleo <mehendaleomkar@gmail.com>
2021-12-06 14:16:17 -0500

    fixed post route, forms etc

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,10 +1,26 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
-from app.models import Post, Image
+from app.models import Post, Image, db
+from app.forms import CreatePostForm, CreateImage
 
 post_routes = Blueprint('posts', __name__)
 
-@post_routes.route('/posts/new', methods=['POST'])
-@login_required
+@post_routes.route('/new', methods=['POST'])
 def createPost():
-    form = 
+    form = CreatePostForm()
+    # form_image = CreateImage()
+    print(form)
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        # print(form_post)
+        new_post = Post(
+            user_id=form.data['user_id'],
+            content=form.data['content'],
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        print(new_post.to_dict())
+        return 'ok'
+    else:
+        print('bad data')
+        return 'bad data'
--- app/forms/create_post_form.py
+++ app/forms/create_post_form.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired, ValidationError
 
-class CreatePost(FlaskForm):
+class CreatePostForm(FlaskForm):
     user_id = IntegerField('user_id', validators=[DataRequired()])
     content = StringField('content')
 
