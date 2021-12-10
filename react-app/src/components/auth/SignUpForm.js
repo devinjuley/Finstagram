d9650e2ba91fa130df676de0c282a3fb4b47c9da
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2021-12-10 15:09:48 -0800

    cancel sign up form page button added to redirect to home page is user decides to not sign up

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -61,78 +61,83 @@ const SignUpForm = () => {
   }
 
   return (
-    <form onSubmit={onSignUp}>
+    <div>
+      <form onSubmit={onSignUp}>
+        <div>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+        <div>
+          <label>First Name</label>
+          <input
+            type='text'
+            name='first_name'
+            onChange={updateFirstName}
+            value={first_name}
+          ></input>
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input
+            type='text'
+            name='last_name'
+            onChange={updateLastName}
+            value={last_name}
+          ></input>
+        </div>
+        <div>
+          <label>Load Profile Image</label>
+          <input
+            type='text'
+            name='profile_image_url'
+            onChange={updateProfileImgUrl}
+            value={profile_image_url}
+          ></input>
+        </div>
+        <div>
+          <label>User Name</label>
+          <input
+            type='text'
+            name='username'
+            onChange={updateUsername}
+            value={username}
+          ></input>
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type='text'
+            name='email'
+            onChange={updateEmail}
+            value={email}
+          ></input>
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type='password'
+            name='password'
+            onChange={updatePassword}
+            value={password}
+          ></input>
+        </div>
+        <div>
+          <label>Repeat Password</label>
+          <input
+            type='password'
+            name='repeat_password'
+            onChange={updateRepeatPassword}
+            value={repeatPassword}
+            required={true}
+          ></input>
+        </div>
+        <button type='submit'>Sign Up</button>
+      </form>
       <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+        <button onClick={(e) => history.push('/')}>Cancel</button>
       </div>
-      <div>
-        <label>First Name</label>
-        <input
-          type='text'
-          name='first_name'
-          onChange={updateFirstName}
-          value={first_name}
-        ></input>
-      </div>
-      <div>
-        <label>Last Name</label>
-        <input
-          type='text'
-          name='last_name'
-          onChange={updateLastName}
-          value={last_name}
-        ></input>
-      </div>
-      <div>
-        <label>Load Profile Image</label>
-        <input
-          type='text'
-          name='profile_image_url'
-          onChange={updateProfileImgUrl}
-          value={profile_image_url}
-        ></input>
-      </div>
-      <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
+    </div>
   );
 };
 
