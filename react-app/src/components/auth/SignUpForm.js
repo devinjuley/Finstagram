0806e5c38f3e7f0197382983ebf1da3b2fff042f
mehendaleo <mehendaleomkar@gmail.com>
2021-12-11 15:23:29 -0500

    signup css done

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Redirect, useHistory } from 'react-router-dom';
 import { signUp } from '../../store/session';
+import './signupform.css';
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
@@ -61,82 +62,105 @@ const SignUpForm = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={onSignUp}>
-        <div>
-          {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-          ))}
+    <div className='signup-main-om'>
+      <div id='signup-form-container'>
+        <div className='signup-finstagram-om'>
+          Finstagram
         </div>
-        <div>
-          <label>First Name</label>
-          <input
-            type='text'
-            name='first_name'
-            onChange={updateFirstName}
-            value={first_name}
-          ></input>
-        </div>
-        <div>
-          <label>Last Name</label>
-          <input
-            type='text'
-            name='last_name'
-            onChange={updateLastName}
-            value={last_name}
-          ></input>
-        </div>
-        <div>
-          <label>Load Profile Image</label>
-          <input
-            type='text'
-            name='profile_image_url'
-            onChange={updateProfileImgUrl}
-            value={profile_image_url}
-          ></input>
-        </div>
-        <div>
-          <label>User Name</label>
-          <input
-            type='text'
-            name='username'
-            onChange={updateUsername}
-            value={username}
-          ></input>
-        </div>
-        <div>
-          <label>Email</label>
-          <input
-            type='text'
-            name='email'
-            onChange={updateEmail}
-            value={email}
-          ></input>
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            type='password'
-            name='password'
-            onChange={updatePassword}
-            value={password}
-          ></input>
-        </div>
-        <div>
-          <label>Repeat Password</label>
-          <input
-            type='password'
-            name='repeat_password'
-            onChange={updateRepeatPassword}
-            value={repeatPassword}
-            required={true}
-          ></input>
-        </div>
-        <button type='submit'>Sign Up</button>
-      </form>
-      <div>
-        <button onClick={(e) => history.push('/')}>Cancel</button>
+        <h2 className='signup-welcome-om'>
+          Sign up to see photos from your friends.
+        </h2>
+        <form onSubmit={onSignUp}>
+          <div>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <div>
+            {/* <label>First Name</label> */}
+            <input
+              type='text'
+              name='first_name'
+              onChange={updateFirstName}
+              value={first_name}
+              placeholder='First Name'
+              className='signup-form-input-om'
+            ></input>
+          </div>
+          <div>
+            {/* <label>Last Name</label> */}
+            <input
+              type='text'
+              name='last_name'
+              onChange={updateLastName}
+              value={last_name}
+              placeholder='Last Name'
+              className='signup-form-input-om'
+            ></input>
+          </div>
+          <div>
+            {/* <label>Load Profile Image</label> */}
+            <input
+              type='text'
+              name='profile_image_url'
+              onChange={updateProfileImgUrl}
+              value={profile_image_url}
+              placeholder='Load Profile Image'
+              className='signup-form-input-om'
+            ></input>
+          </div>
+          <div>
+            {/* <label>User Name</label> */}
+            <input
+              type='text'
+              name='username'
+              onChange={updateUsername}
+              value={username}
+              placeholder='User Name'
+              className='signup-form-input-om'
+            ></input>
+          </div>
+          <div>
+            {/* <label>Email</label> */}
+            <input
+              type='text'
+              name='email'
+              onChange={updateEmail}
+              value={email}
+              placeholder='Email'
+              className='signup-form-input-om'
+            ></input>
+          </div>
+          <div>
+            {/* <label>Password</label> */}
+            <input
+              type='password'
+              name='password'
+              onChange={updatePassword}
+              value={password}
+              placeholder='Password'
+              className='signup-form-input-om'
+            ></input>
+          </div>
+          <div>
+            {/* <label>Repeat Password</label> */}
+            <input
+              type='password'
+              name='repeat_password'
+              onChange={updateRepeatPassword}
+              value={repeatPassword}
+              required={true}
+              placeholder='Repeat Password'
+              className='signup-form-input-om'
+            ></input>
+          </div>
+          <button type='submit' className='signup-buttons-om'>Sign Up</button>
+        </form>
       </div>
+        <div className='login-redirect-parent'>
+          <span>Have an account?</span>
+          <button onClick={(e) => history.push('/')} className='login-redirect-button-om'>Log in</button>
+        </div>
     </div>
   );
 };
