dec8677dcc0babba68ea9f902453e5e0eaa392ba
devinjuley <devinjuley@gmail.com>
2021-12-10 16:49:28 -0800

    finished main feed page

--- react-app/src/components/MainFeed/MainFeed.js
+++ react-app/src/components/MainFeed/MainFeed.js
@@ -28,7 +28,7 @@ export const MainFeedTile = ({ post }) => {
                 <img src={post?.images[0]?.image_url} alt='post' onClick={() => setShowModal(true)} className='single-post-image-mainfeed-dj' />
                 <div className='usename-and-comment-mainfeed-dj'>
                     <a href={`/users/${post?.user?.id}`} className='single-post-username-mainfeed-dj'>{post?.user?.username}</a>
-                    <span>{post?.content}</span>
+                    <span className='mainfeed-caption-dj'> {post?.content}</span>
                 </div>
                 <div onClick={() => setShowModal(true)} className='view-all-comments-mainfeed-dj'>View all comments</div>
                 <CreateCommentForm post={post} showModal={showModal} setShowModal={setShowModal} />
@@ -48,7 +48,7 @@ function MainFeed() {
     const sessionUser = useSelector(state => state.session.user)
     const follows = useSelector(state => state.follows)
     useSelector(state => state.posts)
-
+    const followsArr = Object.assign([], follows)
 
     useEffect(() => {
         dispatch(getAllPostsThunk())
@@ -66,12 +66,69 @@ function MainFeed() {
     posts_arr.reverse()
 
     return (
-        <div>
-            {posts_arr.map(post => (
-                <div key={post.id}>
-                    <MainFeedTile post={post} />
+        <div className='mainfeed-parent-div-dj'>
+            <div>
+                {posts_arr.map(post => (
+                    <div key={post.id}>
+                        <MainFeedTile post={post} />
+                    </div>
+                ))}
+            </div>
+            <div className='follows-div-parent-dj'>
+                <div className='follows-div-dj'>
+                    <div className='session-user-logged-in-dj'>
+                        <img src={sessionUser?.profile_image_url} className='session-user-profile-photo-mainfeed-dj' />
+                        <a href={`/users/${sessionUser?.id}`} className='session-user-username-dj'>{sessionUser?.username}</a>
+                    </div>
+                    <div className='people-you-follow-dj'>People you follow</div>
+                    <div className='people-you-follow-mapping-dj'>
+                        {followsArr.map(person => (
+                            <div className='person-you-are-following-dj'>
+                                <img src={person.profile_image_url} className='commented-user-profile-image-dj' />
+                                <div>
+                                    <a href={`/users/${person.id}`} className='username-inside-follow-list-dj'>{person.username}</a>
+                                    <div className='firstname-lastname-follow-dj'>{person.first_name} {person.last_name}</div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className='footer-parent-div-dj'>
+                        <div className='footer'>
+                            <a href='/' className='my-name'>
+                                Omkar Mehendale
+                            </a>
+                            <a href='https://www.linkedin.com/in/omkar-mehendale-4a8879153/' className='linked-in'>
+                                LinkedIn
+                            </a>
+                            <a href='https://github.com/mehendaleo' className='git-hub'>
+                                GitHub
+                            </a>
+                        </div>
+                        <div className='footer'>
+                            <a href='/' className='my-name'>
+                                Tanner Hladek
+                            </a>
+                            <a href='https://www.linkedin.com/in/tannerhladek/' className='linked-in'>
+                                LinkedIn
+                            </a>
+                            <a href='https://github.com/tannerhladek' className='git-hub'>
+                                GitHub
+                            </a>
+                        </div>
+                        <div className='footer'>
+                            <a href='/' className='my-name'>
+                                Devin Juley
+                            </a>
+                            <a href='https://www.linkedin.com/in/devin-juley-6b4847149/' className='linked-in'>
+                                LinkedIn
+                            </a>
+                            <a href='https://github.com/devinjuley' className='git-hub'>
+                                GitHub
+                            </a>
+                        </div>
+                    </div>
                 </div>
-            ))}
+            </div>
         </div>
     )
 }
