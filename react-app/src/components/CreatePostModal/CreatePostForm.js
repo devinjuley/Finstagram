f1d7a7c5ff538b68529b90606179a3c1383476df
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2022-01-19 15:14:03 -0800

    added loading graphic funstionality

--- react-app/src/components/CreatePostModal/CreatePostForm.js
+++ react-app/src/components/CreatePostModal/CreatePostForm.js
@@ -12,6 +12,7 @@ const CreatePostForm = ({ hideForm }) => {
    const sessionUser = useSelector(state => state.session.user);
    const [caption, setCaption] = useState('');
    const [post_image, setPostImage] = useState(null);
+   const [loading, setLoading] = useState(false);
    const [errors, setErrors] = useState([]);
 
    const handleSubmit = async (e) => {
@@ -21,17 +22,19 @@ const CreatePostForm = ({ hideForm }) => {
       //    content: caption,
       //    // image_url,
       // };
+      setLoading(true);
       const formData = new FormData();
       formData.append('user_id', sessionUser.id);
       formData.append('content', caption);
       formData.append('post_image', post_image);
 
-
       setErrors([]);
       let data = await dispatch(createNewPost(formData))
       if (!data) {
-         hideForm()
+         hideForm();
+         setLoading(false);
       } else {
+         setLoading(false);
          setErrors(data)
       }
    };
@@ -39,46 +42,58 @@ const CreatePostForm = ({ hideForm }) => {
 
    return (
       <div className='make-a-post-grandparent-dj'>
-         <div className='make-a-post-parent-om'>
-            <div className='make-a-post-finstagram-om'>
-               Make a post!
-            </div>
-            <form onSubmit={handleSubmit} className='make-a-post-form-dj'>
-               <div className='create-error-container-om'>
-                  {errors.map((error, ind) => {
-                     const errorMessage = error.split(': ')[1]
-                     return (
-                        <div key={ind} className='error-message-text-th'>
-                           {errorMessage}
-                        </div>
-                     )
-                  })}
+         {loading && (
+            <div className='make-a-post-parent-om'>
+               {/* <div className='make-a-post-finstagram-om'>
+                  Loading
+               </div> */}
+               <div>
+                  <img src='https://finstagram-social-bucket.s3.us-west-1.amazonaws.com/loading.gif' alt='loading'/>
                </div>
-               {/* <input type='file' /> */}
-               <div className='make-a-post-form-dj'>
-                  <div>
-                     <input
-                        type='file'
-                        // value={image_url}
-                        accept='image/*'
-                        onChange={(e) => setPostImage(e.target.files[0])}
-                        // placeholder='Provide an image URL'
-                        className='create-form-input-om'
-                        name='post_image'
+            </div>
+         )}
+         {!loading && (
+            <div className='make-a-post-parent-om'>
+               <div className='make-a-post-finstagram-om'>
+                  Make a post!
+               </div>
+               <form onSubmit={handleSubmit} className='make-a-post-form-dj'>
+                  <div className='create-error-container-om'>
+                     {errors.map((error, ind) => {
+                        const errorMessage = error.split(': ')[1]
+                        return (
+                           <div key={ind} className='error-message-text-th'>
+                              {errorMessage}
+                           </div>
+                        )
+                     })}
+                  </div>
+                  {/* <input type='file' /> */}
+                  <div className='make-a-post-form-dj'>
+                     <div>
+                        <input
+                           type='file'
+                           // value={image_url}
+                           accept='image/*'
+                           onChange={(e) => setPostImage(e.target.files[0])}
+                           // placeholder='Provide an image URL'
+                           className='create-form-input-om'
+                           name='post_image'
+                        />
+                     </div>
+                     <textarea
+                        value={caption}
+                        placeholder='Add a caption to this photo!'
+                        onChange={(e) => setCaption(e.target.value)}
+                        className='create-form-input-textarea-om'
                      />
                   </div>
-                  <textarea
-                     value={caption}
-                     placeholder='Add a caption to this photo!'
-                     onChange={(e) => setCaption(e.target.value)}
-                     className='create-form-input-textarea-om'
-                  />
-               </div>
-               <button type='submit' className='signup-buttons-om'>
-                  Submit
-               </button>
-            </form>
-         </div>
+                  <button type='submit' className='signup-buttons-om'>
+                     Submit
+                  </button>
+               </form>
+            </div>
+         )}
       </div>
    );
 };
