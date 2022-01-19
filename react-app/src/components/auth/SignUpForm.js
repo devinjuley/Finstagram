b0a106e7e656cd7fcdd0cdb579361bb810d296cc
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2022-01-19 15:47:53 -0800

    added sign-up loading image

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -13,6 +13,7 @@ const SignUpForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
+  const [loading, setLoading] = useState(true);
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
   const history = useHistory();
@@ -20,6 +21,7 @@ const SignUpForm = () => {
   const onSignUp = async (e) => {
     e.preventDefault();
     if (password === repeatPassword) {
+      setLoading(true);
       const formData = new FormData();
       formData.append("profile_image", profile_image);
       formData.append("first_name", first_name);
@@ -31,9 +33,11 @@ const SignUpForm = () => {
 
       const data = await dispatch(signUp(formData));
       if (data) {
+        setLoading(false);
         setErrors([data])
         return
       }
+      setLoading(false);
       return history.push('/');
     } else {
       setErrors(['Password: Passwords do not match'])
@@ -75,110 +79,121 @@ const SignUpForm = () => {
   return (
     <div className='mega-parent-of-sign-up-dj'>
       <div className='signup-main-om'>
-        <div className='signup-form-container'>
-          <div className='signup-finstagram-om'>
-            Finstagram
+        {loading && (
+          <div className='signup-form-container'>
+            <div className='loading-img-container'>
+              <img src='https://finstagram-social-bucket.s3.us-west-1.amazonaws.com/loading.gif' alt='loading' />
+            </div>
           </div>
-          <h2 className='signup-welcome-om'>
-            Sign up to see photos from your friends.
-          </h2>
-          <div className='signup-error-messages-container-th'>
-            {errors.map((error, ind) => {
-              // const errorMessage = error.split(': ')[1]
-              return (
-                <div key={ind} className='error-message-text-th'>
-                  {error}
+        )}
+        {!loading && (
+          <>
+            <div className='signup-form-container'>
+              <div className='signup-finstagram-om'>
+                Finstagram
+              </div>
+              <h2 className='signup-welcome-om'>
+                Sign up to see photos from your friends.
+              </h2>
+              <div className='signup-error-messages-container-th'>
+                {errors.map((error, ind) => {
+                  // const errorMessage = error.split(': ')[1]
+                  return (
+                    <div key={ind} className='error-message-text-th'>
+                      {error}
+                    </div>
+                  )
+                })}
+              </div>
+              <form onSubmit={onSignUp}>
+                <div>
+                  {/* <label>First Name</label> */}
+                  <input
+                    type='text'
+                    name='first_name'
+                    onChange={updateFirstName}
+                    value={first_name}
+                    placeholder='First Name'
+                    className='signup-form-input-om'
+                  ></input>
                 </div>
-              )
-            })}
-          </div>
-          <form onSubmit={onSignUp}>
-            <div>
-              {/* <label>First Name</label> */}
-              <input
-                type='text'
-                name='first_name'
-                onChange={updateFirstName}
-                value={first_name}
-                placeholder='First Name'
-                className='signup-form-input-om'
-              ></input>
+                <div>
+                  {/* <label>Last Name</label> */}
+                  <input
+                    type='text'
+                    name='last_name'
+                    onChange={updateLastName}
+                    value={last_name}
+                    placeholder='Last Name'
+                    className='signup-form-input-om'
+                  ></input>
+                </div>
+                <div>
+                  {/* <label>Load Profile Image</label> */}
+                  <input
+                    type='file'
+                    name='profile_image'
+                    accept="image/*"
+                    onChange={updateProfileImg}
+                    // value={profile_image}
+                    placeholder='Upload a profile image'
+                    className='signup-form-input-om'
+                  ></input>
+                </div>
+                <div>
+                  {/* <label>User Name</label> */}
+                  <input
+                    type='text'
+                    name='username'
+                    onChange={updateUsername}
+                    value={username}
+                    placeholder='User Name'
+                    className='signup-form-input-om'
+                  ></input>
+                </div>
+                <div>
+                  {/* <label>Email</label> */}
+                  <input
+                    type='text'
+                    name='email'
+                    onChange={updateEmail}
+                    value={email}
+                    placeholder='Email'
+                    className='signup-form-input-om'
+                  ></input>
+                </div>
+                <div>
+                  {/* <label>Password</label> */}
+                  <input
+                    type='password'
+                    name='password'
+                    onChange={updatePassword}
+                    value={password}
+                    placeholder='Password'
+                    className='signup-form-input-om'
+                  ></input>
+                </div>
+                <div>
+                  {/* <label>Repeat Password</label> */}
+                  <input
+                    type='password'
+                    name='repeat_password'
+                    onChange={updateRepeatPassword}
+                    value={repeatPassword}
+                    required={true}
+                    placeholder='Repeat Password'
+                    className='signup-form-input-om'
+                  ></input>
+                </div>
+                <button type='submit' className='signup-buttons-om'>Sign Up</button>
+              </form>
             </div>
-            <div>
-              {/* <label>Last Name</label> */}
-              <input
-                type='text'
-                name='last_name'
-                onChange={updateLastName}
-                value={last_name}
-                placeholder='Last Name'
-                className='signup-form-input-om'
-              ></input>
+            <div className='login-redirect-parent'>
+              <span>Have an account?</span>
+              <button onClick={(e) => history.push('/')} className='login-redirect-button-om'>Log in</button>
             </div>
-            <div>
-              {/* <label>Load Profile Image</label> */}
-              <input
-                type='file'
-                name='profile_image'
-                accept="image/*"
-                onChange={updateProfileImg}
-                // value={profile_image}
-                placeholder='Upload a profile image'
-                className='signup-form-input-om'
-              ></input>
-            </div>
-            <div>
-              {/* <label>User Name</label> */}
-              <input
-                type='text'
-                name='username'
-                onChange={updateUsername}
-                value={username}
-                placeholder='User Name'
-                className='signup-form-input-om'
-              ></input>
-            </div>
-            <div>
-              {/* <label>Email</label> */}
-              <input
-                type='text'
-                name='email'
-                onChange={updateEmail}
-                value={email}
-                placeholder='Email'
-                className='signup-form-input-om'
-              ></input>
-            </div>
-            <div>
-              {/* <label>Password</label> */}
-              <input
-                type='password'
-                name='password'
-                onChange={updatePassword}
-                value={password}
-                placeholder='Password'
-                className='signup-form-input-om'
-              ></input>
-            </div>
-            <div>
-              {/* <label>Repeat Password</label> */}
-              <input
-                type='password'
-                name='repeat_password'
-                onChange={updateRepeatPassword}
-                value={repeatPassword}
-                required={true}
-                placeholder='Repeat Password'
-                className='signup-form-input-om'
-              ></input>
-            </div>
-            <button type='submit' className='signup-buttons-om'>Sign Up</button>
-          </form>
-        </div>
-        <div className='login-redirect-parent'>
-          <span>Have an account?</span>
-          <button onClick={(e) => history.push('/')} className='login-redirect-button-om'>Log in</button>
-        </div>
+          </>
+        )}
       </div>
     </div>
   );
